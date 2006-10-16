e6416a1c19eaa51b6a16969ad93721f31237619c
phurley <phurley@ff1affdc-b21f-0410-917a-f2b9ae4a3058>
2006-10-16 02:01:01 +0000

    FrameJournal now working with Madeleine
    git-svn-id: svn://rubyforge.org/var/svn/stompserver/trunk@9 ff1affdc-b21f-0410-917a-f2b9ae4a3058

--- lib/frame_journal.rb
+++ lib/frame_journal.rb
@@ -10,43 +10,98 @@
 #
 # When the size of a journal file exceeds its limit
 
-require 'mockfs'
+require 'madeleine'
+require 'madeleine/automatic'
+
+class MadFrameJournal
+  include Madeleine::Automatic::Interceptor
+  attr_reader :frames
+  automatic_read_only :frames
+
+  def initialize
+    @frames = {}
+  end
+
+  def add(msgid, frame)
+    @frames[msgid] = frame
+  end
+
+  def delete(msgid, frame)
+    @frames.delete(msgid)
+  end
+
+  def clear
+    @frames.clear
+  end
+  
+  automatic_read_only :lookup
+  def lookup(msgid)
+    @frames[msgid]
+  end
+end
 
 class FrameJournal
-  Struct.new("FJHeader", :status, :expires, :file)
-    
-  def initialize(directory, size_limit = 2**21)
-    @index = {}
-    @dir = directory
-    MockFS.file_utils.mkpath(@dir) unless MockFS.file.directory?(@dir)
-
-    recover    
-  end
-  
-  def journal(msgid, expires, msg)
-    
-  end
-  
-  def release(msg)
-  end
-  
-  def pending
-  end
-  
-  def rollover
-  end
-
-  def recover  
-    @jnum = -1
-    MockFS.dir.glob(File.join(@dir, 'fjf*.dat')).each do |jf|
-      if jf =~ /fjf(\d*).dat/
-        # todo process these records, collecting headers
-        # delete "empty" files
-        @jnum = [@jnum, $1.to_i].max
-      end
+  def initialize(directory='frame-journal', snap_freq = 60 * 5)
+    @mad = AutomaticSnapshotMadeleine.new(directory) do
+      MadFrameJournal.new
     end
     
-    @jnum += 1
-    @journal = MockFS.file.new(File.join(@dir, "fjf%04d.dat" % @jnum), 'wb')
+    # always snap on startup, in case we had an previous failure
+    @modified = true
+    Thread.new(@mad, snap_freq) do |mad, freq|
+      puts "0. Autosnap #{freq}/#{@modified}"
+      while true
+        sleep(freq)
+        puts "Autosnap #{freq}/#{@modified}"
+        mad.take_snapshot if @modified
+        @modified = false
+      end
+    end
+  end
+  
+  def []=(msgid, frame)
+    @modified = true
+    @mad.system.add(msgid, frame)
+  end
+  
+  def [](msgid)
+    @mad.system.lookup(msgid)
+  end
+  
+  def delete(msgid)
+    @modified = true
+    @mad.system.delete(msgid)
+  end
+  
+  def keys
+    @mad.system.frames.keys
+  end
+  
+  def clear
+    @modified = true
+    @mad.system.clear
+  end
+end
+
+fj = FrameJournal.new('fj', 3)
+until ARGV.empty?
+  case cmd = ARGV.shift
+  when "keys"
+    puts fj.keys.inspect
+  when "dump"
+    fj.keys.each do |key|
+      puts "#{key}: #{fj[key]}"
+    end
+  when "show"
+    key = ARGV.shift
+    puts "#{key}: #{fj[key]}"
+  when "add"
+    key = ARGV.shift
+    val = ARGV.shift
+    fj[key] = val
+  when "sleep"
+    sleep ARGV.shift.to_i
+  when "delete"
+    fj.delete(ARGV.shift)
   end
 end
--- test/test_frame_journal.rb
+++ test/test_frame_journal.rb
@@ -2,18 +2,25 @@ require 'frame_journal'
 require 'test/unit' unless defined? $ZENTEST and $ZENTEST
 
 class TestFrameJournal < Test::Unit::TestCase
-  def init
+  def setup
     MockFS.mock = true   
+    MockFS.flush
+    MockFS.fill_path '.'
   end
   
   def test_directory_create
-    fj = FrameJournal.new('/foo/bar')
-    assert(MockFS.file.exist?('/foo/bar'))
-    assert(!MockFS.file.exist?('bar'))
-    assert(MockFS.file.directory?('/foo'))
-    assert(MockFS.file.directory?('/foo/bar'))
+    MockFS.mock_file_system.flush
+    MockFS.fill_path '.'
+    p
     
-    assert(MockFS.file.exist?('/foo/bar/fjf0001.dat2'))
+    fj = FrameJournal.new('foo/bar')
+    assert(MockFS.file.exist?('foo/bar'))
+    assert(!MockFS.file.exist?('bar'))
+    assert(MockFS.file.directory?('foo'))
+    assert(MockFS.file.directory?('foo/bar'))
+    
+    assert(MockFS.file.exist?('foo/bar/fjf0000.dat'))
+    assert(!MockFS.file.exist?('foo/bar/fjf0001.dat'))
   end
   
   def test_journal
@@ -28,4 +35,3 @@ class TestFrameJournal < Test::Unit::TestCase
   def test_rollover
   end
 end
-
