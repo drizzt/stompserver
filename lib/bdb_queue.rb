8767880d15562c1ca16ec39221cd22315a88efb1
snacktime <snacktime@ff1affdc-b21f-0410-917a-f2b9ae4a3058>
2006-10-20 05:39:08 +0000

    Changed the file backing store to a berkeleydb hash. Better performance and easier to manage.
    git-svn-id: svn://rubyforge.org/var/svn/stompserver/branches/snacktime@50 ff1affdc-b21f-0410-917a-f2b9ae4a3058

--- lib/bdb_queue.rb
+++ lib/bdb_queue.rb
@@ -20,26 +20,27 @@ class BDBQueue
     @active.close
   end
 
+
   def open_queue(dest)
     @queues[dest] = Hash.new
     queue_name = dest.gsub('/', '_')
-    queue_files = @directory + '/' + queue_name
-    queue_bdb = @directory + '/' + queue_name + '.db'
-    Dir.mkdir(queue_files) unless File.directory?(queue_files)
-    @queues[dest][:dbh] = BDB::Queue.new("#{queue_bdb}", nil, "a")
-    @queues[dest][:queue_files] = queue_files
-    @queues[dest][:queue_bdb] = queue_bdb
+    queue_dbname = @directory + '/' + queue_name + '_queue.db'
+    store_dbname = @directory + '/' + queue_name + '_store.db'
+    @queues[dest][:queue] = BDB::Queue.new("#{queue_dbname}", nil, "a")
+    @queues[dest][:store] = BDB::Hash.new("#{store_dbname}", nil, "a")
+    @queues[dest][:queue_dbname] = queue_dbname
+    @queues[dest][:store_dbname] = store_dbname
     @active[dest] = true
-    p "Queue #{dest} opened files=#{@queues[dest][:queue_files]} dbfile=#{@queues[dest][:queue_bdb]}" if $DEBUG
   end
 
 
   def close_queue(dest)
-    qsize = @queues[dest][:dbh].size
-    @queues[dest][:dbh].close
+    qsize = @queues[dest][:queue].size
+    @queues[dest][:queue].close
+    @queues[dest][:store].close
     if qsize == 0
-      File.directory?(@queues[dest][:queue_files]) if  Dir.delete(@queues[dest][:queue_files])
-      File.delete(@queues[dest][:queue_bdb]) if File.exists?(@queues[dest][:queue_bdb])
+      File.delete(@queues[dest][:queue_dbname]) if File.exists?(@queues[dest][:queue_dbname])
+      File.delete(@queues[dest][:store_dbname]) if File.exists?(@queues[dest][:store_dbname])
       @active.delete(dest)
       p "Removing queue #{dest}"
     else
@@ -50,18 +51,16 @@ class BDBQueue
 
   def enqueue(dest,frame)
     open_queue(dest) unless @queues.has_key?(dest)
-    msgid = @queues[dest][:dbh].push dest
-    file = @queues[dest][:queue_files] + '/' + msgid.to_s
+    msgid = @queues[dest][:queue].push dest
     frame.headers['message-id'] = msgid.to_s
-    File.open(file,'w') {|f| f.write(frame)}
+    @queues[dest][:store][msgid[0]] = frame
   end
 
   def dequeue(dest)
     open_queue(dest) unless @queues.has_key?(dest)
-    if qitem = @queues[dest][:dbh].shift
-      file = @queues[dest][:queue_files] + '/' + qitem[0].to_s
-      frame_text = File.read(file)
-      File.delete(file)
+    if queue = @queues[dest][:queue].shift
+      frame_text = @queues[dest][:store][queue[0]]
+      @queues[dest][:store].delete(queue[0])
       @sfr << frame_text
       @sfr.frames.shift
     else
