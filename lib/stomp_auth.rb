03d326e445c8a9f6120786ef7c5deb0f9729ea51
snacktime <snacktime@ff1affdc-b21f-0410-917a-f2b9ae4a3058>
2006-10-23 06:53:47 +0000

    Oops forgot to commit stomp_auth
    git-svn-id: svn://rubyforge.org/var/svn/stompserver/branches/snacktime@72 ff1affdc-b21f-0410-917a-f2b9ae4a3058

---
+++ lib/stomp_auth.rb
@@ -0,0 +1,20 @@
+
+class StompAuth
+attr_accessor :authorized
+
+  def initialize(passfile='.passwd')
+    @passfile = passfile
+    @authorized = Hash.new
+    if File.exists?(@passfile)
+      file = File.read(@passfile)
+      file.split(/\n/).each do |data|
+        next if data =~/^\s*#/
+        data.gsub(/\s/,'')
+        if data =~ /^\s*(\S+)\s*:\s*(.*?)\s*$/
+          @authorized[$1] = $2
+        end
+      end
+    end
+    p "Authorized users #{@authorized.keys}" if $DEBUG
+  end
+end
